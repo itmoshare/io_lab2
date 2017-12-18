61d1fc03dce8094a3821b456c78228daa3be3aab
Dmitriy Mokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-12-18 23:58:25 +0300

    Last commit

--- io_lab2/io_lab2.sdk/test/src/helloworld.c
+++ io_lab2/io_lab2.sdk/test/src/helloworld.c
@@ -56,6 +56,8 @@
 #define AXI_UARLITE_ADDRESS 0x40600000
 #define MY_BASE_ADDRESS 0xC0000000
 
+u32 t_old = 0;
+
 int main()
 {
     init_platform();
@@ -66,6 +68,30 @@ int main()
 	// Timer 1 start
 	Xil_Out32(MY_BASE_ADDRESS + 0x0, 1000);
 	Xil_Out32(MY_BASE_ADDRESS + 0x8, 0x2);
+
+	// AXI Timer configuration
+	Xil_Out32(TIMER_ADDRESS, 0x99);
+
+	while(1)
+	{
+		// timer 1 control register data
+		u32 c1 = Xil_In32(TIMER_ADDRESS);
+
+		// capture is occurred
+		if (c1 & 0x100)
+		{
+			// timer 1 load register data
+			u32 t = Xil_In32(TIMER_ADDRESS + 0x04);
+
+			if (t_old != 0)
+				Xil_Out32(GPIO_ADDRESS, t - t_old);
+
+			t_old = t;
+
+			// clear TINT flag
+			Xil_Out32(TIMER_ADDRESS, 0x199);
+		}
+	}
 
     //print("N");
     //char t = inbyte();
